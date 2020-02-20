82f3a7e506b0f8eb6e5e547d229422882dff46a1
Ahmed <imaneben8@gmail.com>
2020-02-20 22:04:42 +0000

    added pics

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -2,7 +2,14 @@ import React from "react";
 import Product from "./product";
 
 const ProductList = () => {
-  const shoeList = ["prada", "gucci", "Lv", "lacoste", "Nike"];
+  const shoeList = [
+    "prada",
+    "gucci",
+    "louis vuitton",
+    "lacoste",
+    "Nike",
+    "Adidas"
+  ];
   return (
     <div className="ProductList">
       {shoeList.map(shoe => (
--- src/components/product.js
+++ src/components/product.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Product = props => {
-  return <div>{props.name}</div>;
+  return (
+    <div className="Product">
+      <div
+        style={{
+          backgroundImage: `url('/adidas.jpg')`
+        }}
+      >
+        hugy
+      </div>
+      {props.name}
+    </div>
+  );
 };
 
 export default Product;
