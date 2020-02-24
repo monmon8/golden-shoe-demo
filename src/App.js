de82220ce4a94267b1ed242218b5d63308c58861
Ahmed <imaneben8@gmail.com>
2020-02-24 14:27:54 +0000

    renders chatbot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import ProductList from "./components/product-list";
+import Chatbot from "./components/chatbot.js";
 
 function App() {
   return (
     <div className="App">
       <ProductList />
+      <Chatbot />
     </div>
   );
 }
