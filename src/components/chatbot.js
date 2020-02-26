6004e9c10dd1f4e76d73f56935ceba973a906b89
Ahmed <imaneben8@gmail.com>
2020-02-26 10:07:12 +0000

    updating the chatbot

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -22,17 +22,57 @@ function Chatbot(props) {
     {
       id: "Waiting user input for name",
       user: true,
-      trigger: "Displaying options for help"
+      trigger: "Asking what is your query"
     },
     {
-      id: "Displaying options for help",
-      message: "Hi {previousValue}, How Can I help you",
-      trigger: "Done"
+      id: "Asking what is your query",
+      message:
+        "Hi {previousValue}, Please click on one of the following options",
+      trigger: "Displaying options to help"
     },
+    {
+      id: "Displaying options to help",
+      options: [
+        {
+          value: "return process",
+          label: "Return process",
+          trigger: "Show return process"
+        },
+        {
+          value: "delivery dates and time",
+          label: "Delivery dates and time",
+          trigger: "Show delivery date and time process"
+        },
+        {
+          value: "anything else",
+          label: "Anything else",
+          trigger: "Show anything else"
+        }
+      ]
+    },
+    {
+      id: "Show return process",
+      message:
+        "You have 30 days from the shipping date of your order to return your purchase from Goldenshoe free of charge.",
+      end: true
+    },
+    {
+      id: "Show delivery date and time process",
+      message: `Home delivery:
 
+        Standard delivery - 4.95 £ / FREE (Orders over 50 £)
+        
+        The estimated delivery period is 2-4 business days, depending on the delivery address.
+        
+        On the same day of the purchase - 9.95 £
+        
+        The estimated delivery period is 1-2 business days.`,
+      end: true
+    },
     {
-      id: "Done",
-      message: "Have a great day !!",
+      id: "Show anything else",
+      message:
+        "You can call our customer service helpline on 09001919192 from 9am to 5pm",
       end: true
     }
   ];
